5d2413edfa05d96cc60c98aa1a6ab5d801f6c248
Victor_Gostyaikin <gostyaikin@yandex.ru>
2021-09-21 16:30:28 +0300

    first commit

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,26 +1,25 @@
 def bubble_sort(seq):
     """Inefficiently sort the mutable sequence (list) in place.
-        seq MUST BE A MUTABLE SEQUENCE.
-
-        As with list.sort() and random.shuffle this does NOT return 
+    seq MUST BE A MUTABLE SEQUENCE.
+    As with list.sort() and random.shuffle this does NOT return 
     """
     changed = True
+    GAP = 1
     while changed:
         changed = False
-        for i in range(len(seq) - 1):
-            if seq[i] > seq[i+1]:
-                seq[i], seq[i+1] = seq[i+1], seq[i]
+        for item in range(len(seq) - GAP):
+            if seq[item] > seq[item+GAP]:
+                seq[item], seq[item+GAP] = seq[item+GAP], seq[item]
                 changed = True
     return seq
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     """Sample usage and simple test suite"""
-
     from random import shuffle
 
-    testset = list(range(1000))
-    testcase = testset[:] # make a copy
+    testset = list(range(100))
+    testcase = testset[:]
     shuffle(testcase)
-    assert testcase != testset  # we've shuffled it
+    assert testcase != testset
     bubble_sort(testcase)
-    assert testcase == testset  # we've unshuffled it back into a copy
+    assert testcase == testset
